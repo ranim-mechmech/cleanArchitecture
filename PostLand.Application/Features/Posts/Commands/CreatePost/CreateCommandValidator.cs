313e8307e7710c054c3b2f45b8cc7eb1b7f9e029
Ranim Mechmech <ranimmechmech92@gmail.com>
2024-03-14 13:43:39 +0100

    intial commit

---
+++ PostLand.Application/Features/Posts/Commands/CreatePost/CreateCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostLand.Application.Features.Posts.Commands.CreatePost
+{
+    public class CreateCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreateCommandValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(100);
+
+            RuleFor(p => p.Content)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
